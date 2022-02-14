1ebe7fc769b6d7ffbc8993e6401fff043fe7d11e
Underwerse <pvchernoff@gmail.com>
2022-02-14 16:09:10 +0200

    fix: fix navigation error when trying to rotate screen (build.gradle versions navigation component got from google codelabs)

--- app/src/main/java/fi/example/parties/MainActivity.kt
+++ app/src/main/java/fi/example/parties/MainActivity.kt
@@ -11,20 +11,20 @@ import fi.example.parties.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
-//    private lateinit var myObserver: MyObserver
+    private lateinit var myObserver: MyObserver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-//        val binding =
-//            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         val navController = this.findNavController(R.id.myNavHostFragment)
 
         NavigationUI.setupActionBarWithNavController(this,navController)
 
-//        myObserver = MyObserver()
-//        lifecycle.addObserver(myObserver)
+        myObserver = MyObserver()
+        lifecycle.addObserver(myObserver)
 
         appContext = this
     }
